27a8d514c80815a39d6dac05482f2dbc2fb4953b
Raymond <robertkejn@gmail,com>
2023-03-21 21:07:16 +0400

    Initial commit

---
+++ ht_3_9.1/ht_3_9.1.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+template<class T>
+void move_vectors(std::vector<T>& lv, std::vector<T>& rv) {
+    lv = std::move(rv);
+}
+
+int main()
+{
+    std::vector <std::string> one = { "test_string1", "test_string2" };
+    std::vector <std::string> two;
+    std::cout << "[IN]: ";
+    std::for_each(one.begin(), one.end(), [](const std::string& s)->void {std::cout << s << " "; });
+    move_vectors(two, one);
+    std::cout << "\n[OUT]: ";
+    std::for_each(two.begin(), two.end(), [](const std::string& s)->void {std::cout << s << " "; });
+}
